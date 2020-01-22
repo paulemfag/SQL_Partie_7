e5f6b2417463b8c9a144f66e5937c68ce0503ff4
paulemfag <boiteprofagot@gmail.com>
2020-01-22 11:37:52 +0100

    **

--- source.sql
+++ source.sql
@@ -6,8 +6,7 @@ DELETE FROM `languagess` WHERE `language` = 'HTML';
 
 /* exo2 partie7 */
 
-UPDATE `frameworks` SET  frameworks REPLACE('Symfony', 'Symfony2');
-
+UPDATE `frameworks` SET `version` ='Symfony2' WHERE `version` = 'Symfony';
 /* exo3 partie7 */
 
 UPDATE `languagess` SET `versions` = '5.1' WHERE `language` = 'JavaScript' AND `versions`= '5';
